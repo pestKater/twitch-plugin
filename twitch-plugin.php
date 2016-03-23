65e62ae67a7af558a81f2dfca4cbc26ef8a6905d
Halldor Rolandsson <etwas@black-flirt.de>
2016-03-23 18:23:06 +0100

    Included Header

---
+++ twitch-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Twitch Plugin
+ * Plugin URI: https://zockerfurs.de
+ * Description: This Plugin easily enables a embedding-backend to display costumized Twitch-Status on your Wordpress-Page.
+ * Author: Halldor "pestKater" Rolandsson
+ * Version: 0.0.1a
+ * Licence: GPLv3
+ */
+
